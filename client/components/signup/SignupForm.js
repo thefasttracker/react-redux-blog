94896bf243083393c42809ab01943ed7b2637e2a
Oleg Novikov <thefasttracker@gmail.com>
2017-04-22 17:19:30 +0300

    send data to server redux-thunk action

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import timezones from '../../data/timezones'
 import map from 'lodash/map'
+// import axios from 'axios'
 
 export default class SignupForm extends Component {
 	constructor(props) {
@@ -25,7 +26,8 @@ export default class SignupForm extends Component {
 
 	onSubmit(e) {
 		e.preventDefault()
-		console.log(this.state)
+		// axios.post('/api/users', {user: this.state})
+		this.props.userSignupRequest(this.state)
 	}
 
 	render() {
@@ -103,3 +105,8 @@ export default class SignupForm extends Component {
 		)
 	}
 }
+
+SignupForm.propTypes = {
+		userSignupRequest: React.PropTypes.func.isRequired
+}
+
