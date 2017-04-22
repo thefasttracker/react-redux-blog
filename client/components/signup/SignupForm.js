18ec81b308de743c4e60d3445aa7ba4cb5426270
Oleg Novikov <thefasttracker@gmail.com>
2017-04-22 20:57:02 +0300

    add server-side validation

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import timezones from '../../data/timezones'
 import map from 'lodash/map'
+import classnames from 'classnames'
 // import axios from 'axios'
 
 export default class SignupForm extends Component {
@@ -11,7 +12,9 @@ export default class SignupForm extends Component {
 			email: "",
 			password: "",
 			passwordConfirmation: "",
-			timezone: ""
+			timezone: "",
+			errors: {},
+			isLoading: false
 		}
 		this.onChange = this.onChange.bind(this)
 		this.onSubmit = this.onSubmit.bind(this)
@@ -25,12 +28,17 @@ export default class SignupForm extends Component {
 	}
 
 	onSubmit(e) {
+		this.setState({ errors: {}, isLoading: true})
 		e.preventDefault()
 		// axios.post('/api/users', {user: this.state})
-		this.props.userSignupRequest(this.state)
+		this.props.userSignupRequest(this.state).then(
+			() => {},
+			({ data }) => this.setState({ errors: data, isLoading: false })
+		)
 	}
 
 	render() {
+		const { errors } = this.state
 		const options = map(timezones, (val, key) => 
 			<option key={val} value={val}>{key}</option>
 		)
@@ -38,7 +46,7 @@ export default class SignupForm extends Component {
 			<form onSubmit={this.onSubmit}>
 				<h1>Join us!</h1>
 			
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.username})}>
 					<label className="control-label">Username</label>
 					<input
 						onChange={this.onChange}
@@ -47,9 +55,10 @@ export default class SignupForm extends Component {
 						name="username" 
 						className="form-control"
 					/>
+					{errors.username && <span className="help-block">{errors.username}</span>}
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.email})}>
 					<label className="control-label">Email</label>
 					<input
 						onChange={this.onChange}
@@ -58,9 +67,10 @@ export default class SignupForm extends Component {
 						name="email" 
 						className="form-control"
 					/>
+					{errors.email && <span className="help-block">{errors.email}</span>}
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.password})}>
 					<label className="control-label">Password</label>
 					<input
 						onChange={this.onChange}
@@ -69,9 +79,10 @@ export default class SignupForm extends Component {
 						name="password" 
 						className="form-control"
 					/>
+					{errors.password && <span className="help-block">{errors.password}</span>}
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.passwordConfirmation})}>
 					<label className="control-label">Password Confirmation</label>
 					<input
 						onChange={this.onChange}
@@ -80,9 +91,10 @@ export default class SignupForm extends Component {
 						name="passwordConfirmation" 
 						className="form-control"
 					/>
+					{errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.timezone })}>
 					<label className="control-label">Timezone</label>
 					<select
 						onChange={this.onChange}
@@ -93,11 +105,11 @@ export default class SignupForm extends Component {
 						<option value="" disabled>Choose Your Timezone</option>
 						{options}
 					</select>
-
+					{errors.timezone && <span className="help-block">{errors.timezone}</span>}
 				</div>
 
 				<div className="form-group">
-					<button className="btn btn-primary btn-lg">
+					<button disabled={this.state.isLoading} className="btn btn-primary btn-lg">
 						Sign up
 					</button>
 				</div>
--- server/index.js
+++ server/index.js
@@ -1,23 +1,31 @@
-import express from 'express'
-import path from 'path'
-import webpack from 'webpack'
-import webpackMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
-import webpackConfig from '../webpack.config.dev'
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
 
-const app = express()
-const compiler = webpack(webpackConfig)
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
+
+import users from './routes/users';
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+
+const compiler = webpack(webpackConfig);
 
 app.use(webpackMiddleware(compiler, {
-	hot: true,
-	publicPath: webpackConfig.output.publicPath,
-	noInfo: true
-}))
-
-app.use(webpackHotMiddleware(compiler))
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, './index.html'))
-})
+  res.sendFile(path.join(__dirname, './index.html'));
+});
 
-app.listen(3000, () => console.log('server running on localhost:3000'))
+app.listen(3000, () => console.log('Running on localhost:3000'));
